2c76ee9d001c0ca8ffe4bb8277bf736048dd0ce6
yash110905 <pyash4521@gmail.com>
2026-06-25 13:10:40 -0400

    Added course model and user course relationship

---
+++ models/course.py
@@ -0,0 +1,22 @@
+from database import db
+
+
+class Course(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(
+        db.String(100),
+        nullable=False
+    )
+
+    code = db.Column(
+        db.String(50),
+        nullable=False
+    )
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey("user.id"),
+        nullable=False
+    )
--- models/user.py
+++ models/user.py
@@ -1,19 +1,21 @@
 from database import db
 from flask_login import UserMixin
-
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True) 
-
+    id = db.Column(db.Integer, primary_key=True)
+    
+    courses = db.relationship(
+        "Course",
+        backref="owner",
+        lazy=True
+    )
     email = db.Column(
         db.String(100),
         unique=True,
         nullable=False
-    )#Required & unique email
-
+    ) #Required & unique email
     password = db.Column(
         db.String(200),
         nullable=False
-    )#Required password
-    #email
+    ) #Required password
     def __repr__(self):
         return self.email
