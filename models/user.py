38be48d7b257f4c7edf56963e5f3a8b74a3e9bf7
yash110905 <pyash4521@gmail.com>
2026-06-25 13:05:45 -0400

    Implemented user registration and login authentication

--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 from database import db
 from flask_login import UserMixin
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True) 
 
     email = db.Column(
