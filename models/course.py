765eca0dc7d69b70cfe21e87ac228a3a66559c24
yash110905 <pyash4521@gmail.com>
2026-06-25 13:12:00 -0400

    Added course model

--- models/course.py
+++ models/course.py
@@ -3,7 +3,10 @@ from database import db
 
 class Course(db.Model):
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
 
     name = db.Column(
         db.String(100),
